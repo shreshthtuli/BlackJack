790f089904513b083337353def4d1702a7af660a
Shreshth Tuli <shreshthtuli@gmail.com>
2018-10-20 21:43:15 +0530

    update

--- java/Main.java
+++ java/Main.java
@@ -16,7 +16,7 @@ public class Main{
             System.out.println(str + " : " + vi.value.get(str));
         }
 
-        State s = new State("11,11,false,false");
+        State s = new State("16,3,false,false");
         System.out.println(vi.qStar(s, 0));
         System.out.println(vi.qStar(s, 1));
 
@@ -30,11 +30,13 @@ public class Main{
             System.out.println("Value : " + vi.value.get(nxs.getKey().to_string()));
         }
 
+        s = new State("36,3,false,false");
         for(int i = 17; i <= 23; i++){
             System.out.println("Rewardhelper : " + i + " : " + vi.m.dealer_prob.get(new Pair(s.dealer_hand, i)) + " : " + vi.m.rewardHelper(s, i));
         }
 
         System.out.println(vi.qStar(new State("1,2,false,false"), 0));
+        System.out.println(vi.m.getReward(new State("36,3,false,false"), 1));
 
 
     }
